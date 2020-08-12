c3aa5673fddff53d2d752a1ca3656f846f66406c
Nathan Ferreira <ntfm95@gmail.com>
2020-08-11 23:38:19 -0300

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,14 @@
+number = set()
+
+number.add(2)
+number.add(2)
+number.add(3)
+
+print(number)
+
+lottery_values = {3, 5, 13, 6}
+print(lottery_values)
+
+user_values = {3, 5, 11, 2}
+
+print(lottery_values.intersection(user_values))
