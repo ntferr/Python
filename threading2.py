fcdea416a964b5d11d2d607378791f775f533048
Nathan Ferreira <ntfm95@gmail.com>
2021-01-15 09:38:39 -0300

    Update threading2.py

--- threading2.py
+++ threading2.py
@@ -1,1 +1,20 @@
+import logging
+import threading
+import time
 
+def printing_name():
+    na = "Alex"
+    print("Name: ", na)
+
+def printing_age():
+    age = 20
+    print("Age: ", age)
+
+if __name__ == '__main__':
+   x = threading.Thread(target=printing_name)
+   x.start()
+   
+   time.sleep(4)
+
+   y = threading.Thread(target=printing_age)
+   y.start()
